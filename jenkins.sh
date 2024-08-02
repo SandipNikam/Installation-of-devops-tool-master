d946b85fb1fdfe23dafc97eb2fb38dbacc9746f1
sandip <sandipnikam6082@gmail.com>
2024-08-02 17:30:01 +0530

    first commit

---
+++ jenkins.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sudo yum update -y
+
+sudo yum upgrade -y 
+
+sudo yum install java-1.8.0
+
+sudo yum remove java-1.7.0-openjdk
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+sudo yum install jenkins -y
+
+sudo systemctl enable jenkins
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+    
+For Ubuntu
+
+sudo apt update
+
+sudo apt install openjdk-17-jre
+
+java -version
+ 
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install jenkins
+
+sudo systemctl start jenkins.service
+
+sudo systemctl status jenkins
